68eec3184f9342a1236604ea15da7f67c43d4bf2
guohanyu123 <hanyug@clemson.edu>
2015-11-20 20:02:43 -0500

    delete them all

--- redundant/dbconnect.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "mysql1.cs.clemson.edu";
-$user = "jingyam";
-$password = "19910429Mjy@";
-$database = "jingyamdatabase";
-
-
-?>
--- redundant/foobar_submit.php
+++
@@ -1,5 +0,0 @@
-<?php
- // echo $_POST['my_html_input_tag'];
-  echo "<br><br>";
-  //print_r($_POST); 
-?>
